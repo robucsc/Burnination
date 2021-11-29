9243f95123c164922c0137568ee4e58619058355
alachoff-ucsc <74936465+alachoff-ucsc@users.noreply.github.com>
2021-11-29 03:13:57 -0800

    Fixed animations for sprites
    They animate now :)

---
+++ Assets/Scripts/KnightScript.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightScript : MonoBehaviour
+{
+    SpriteRenderer m_SpriteRenderer;
+    Animator m_Animator;
+    public string state = "idle";
+    private float moveSpeed = 0.0f;
+    private float moveTime = 2.0f;
+    private float idleTime = 5.0f;
+    
+    // Awake is called when an instance of this script is created
+    void Awake()
+    {
+        //Get the Animator attached to the GameObject you are intending to animate.
+        m_Animator = gameObject.GetComponent<Animator>();
+
+        // Get the SpriteRenderer attatched to the GameObject you are intending to uhhhh render???
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // establish movement
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
+
+        if (state == "burning")
+        {
+            // move quickly in a direction
+            // apply burning effect
+            // play running animation
+            // destroy eventually
+        }
+        else if (state == "walking")
+        {
+            // move in a direction for moveTime seconds at random speed 
+            moveTime -= Time.deltaTime;
+            if (moveSpeed == 0.0f)
+            {
+                moveSpeed = Random.Range(-1.0f, 1.0f);
+                if (moveSpeed >= 0.0f)
+                {
+                    m_SpriteRenderer.flipX = true;
+                }
+                else
+                {
+                    m_SpriteRenderer.flipX = false; 
+                }
+            }
+
+            if (moveTime <= 0.0)
+            {
+                moveSpeed = -moveSpeed;
+                idleTime = 5.0f;
+                state = "idle";
+            }
+            // play walking animation
+            m_Animator.SetTrigger("walking");
+        }
+        else
+        {
+            // start idletimer, stop moving
+            idleTime -= Time.deltaTime;
+            moveSpeed = 0.0f;
+            if (idleTime <= 0.0)
+            {
+                moveTime = 1.0f;
+                state = "walking";
+            }
+            
+            // play idle animation
+            m_Animator.ResetTrigger("walking");
+        }
+    }
+}
