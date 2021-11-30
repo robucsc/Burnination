ad463f110c2823373763ea2c1d9099f9acb7f8e3
alachoff-ucsc <74936465+alachoff-ucsc@users.noreply.github.com>
2021-11-30 11:32:03 -0800

    Removed HDRP
    Will reinstate its glory upon the Master branch once I merge. Everything else should merge fine now.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -26,7 +26,7 @@ public class Spawner : MonoBehaviour
             // when spawn timer hits 0, restart timer and spawn entity
             if (spawnRate <= 0.0f)
             {
-                Rigidbody2D clone;
+                GameObject clone;
                 clone = Instantiate(myPrefab, spawnerPos + new Vector3(0.0f, 0.5f, Random.Range(-1.0f, -3.0f)), Quaternion.identity);
                 
                 spawnRate = Random.Range(20.0f, 40.0f);
